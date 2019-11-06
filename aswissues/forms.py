e130396c800de017730b7ccbe5b3e1dce20fcf7d
guillezcurra <guillezcurra@gmail.com>
2019-11-06 21:54:16 +0100

    canvis estètics

--- aswissues/forms.py
+++ aswissues/forms.py
@@ -5,7 +5,7 @@ from django.utils.safestring import mark_safe
 class NovaIssueForm(forms.Form):
     titol = forms.CharField(label='Títol', max_length=100)
     # descripcio = forms.CharField(label='Descripció', max_length=1000
-    descripcio = forms.CharField(widget=forms.Textarea)
+    descripcio = forms.CharField(label='Descripció',widget=forms.Textarea)
     tria1 = (('Bug', 'bug'),('Millora', 'millora'),('Proposta', 'proposta'),('Tasca', 'tasca'),)
     tipus = forms.ChoiceField(label='Tipus', widget=forms.Select, choices=tria1)
     tria2 = (('Trivial', 'Trivial'),('Menor', 'menor'), ('Major', 'major'), ('Crítica', 'critica'), ('Bloquejant', 'bloquejant'),)
