ce90b12d5018536d2993fa14a82a4cc715c5d09e
marti.ramon <martiram8@gmail.com>
2019-11-20 20:03:35 +0100

    Assignat amb link a la taula

--- aswissues/tables.py
+++ aswissues/tables.py
@@ -4,16 +4,14 @@ from django_tables2.utils import Accessor
 
 class IssueTable(tables.Table):
     titol = tables.TemplateColumn('<a href= "./issue/{{record.id}}">{{record.titol}}</a>')
-    nameC = tables.Column(verbose_name='Creador', accessor=Accessor('creator.username'))
-    nameA = tables.Column(verbose_name='Assignat a', accessor=Accessor('assignee.username'))
+    Assignat = tables.TemplateColumn('<a href= "./?creator={{record.assignee}}">{{record.assignee}}</a>')
     prioritat = tables.TemplateColumn('<a href= "./?prioritat={{record.prioritat}}">{{record.prioritat}}</a>')
     tipus = tables.TemplateColumn('<a href= "./?tipus={{record.tipus}}">{{record.tipus}}</a>')
     status = tables.TemplateColumn('<a href= "./?tipus={{record.status}}">{{record.status}}</a>')
-    #nameCr = tables.TemplateColumn('<a href= "./?creator='+nameC+'">'+nameC+'</a>')
     data_creacio = tables.TemplateColumn('<a href= "./?tidata_creacio={{record.data_creacio}}">{{record.data_creacio}}</a>')
 
     class Meta:
         model = Issue
         template_name = "django_tables2/bootstrap4.html"
-        fields = ("titol", "tipus", "prioritat", "nameA", "nameC", 'status', "data_creacio")
+        fields = ("titol", "tipus", "prioritat", "Assignat", "status", "data_creacio")
         per_page = 20
