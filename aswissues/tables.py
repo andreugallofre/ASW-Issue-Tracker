e75b7946b7523176c5b6a9250b077e247cc94768
Andreu Gallofré <andreu.gallofre@capside.com>
2019-11-21 12:23:54 +0100

    Change table

--- aswissues/tables.py
+++ aswissues/tables.py
@@ -2,15 +2,6 @@ import django_tables2 as tables
 from .models import Issue, Vote, Watch
 from django_tables2.utils import Accessor
 
-class VotesColumn(tables.TemplateColumn):
-    def render(self, record, table, value, bound_column, **kwargs):
-            return(Vote.objects.filter(issue=record).count())
-
-class WatchersColumn(tables.TemplateColumn):
-    def render(self, record, table, value, bound_column, **kwargs):
-            return(Watch.objects.filter(issue=record).count())
-
-
 class IssueTable(tables.Table):
 
     titol = tables.TemplateColumn('<a href= "./issue/{{record.id}}">{{record.titol}}</a>')
@@ -19,8 +10,16 @@ class IssueTable(tables.Table):
     tipus = tables.TemplateColumn('<a href= "./?tipus={{record.tipus}}">{{record.tipus}}</a>')
     status = tables.TemplateColumn('<a href= "./?status={{record.status}}">{{record.status}}</a>')
     data_creacio = tables.TemplateColumn('<a href= "./?data_creacio={{record.data_creacio}}">{{record.data_creacio}}</a>')
-    votes = VotesColumn('<a href= "/"></a>')
-    watchers = WatchersColumn('<a href= "/"></a>')
+    votes = tables.Column(empty_values=())
+    watchers = tables.Column(empty_values=())
+
+    def render_votes(self, value, record):
+        return Vote.objects.filter(issue=record).count()
+
+    def render_watchers(self, value, record):
+        if self.request.user.is_authenticated:
+            return Watch.objects.filter(issue=record, watcher=self.request.user).count()
+        return 0
 
     class Meta:
         model = Issue
