f502dadff9014b774859a6377ad049825b20bc4a
guillezcurra <guillezcurra@gmail.com>
2019-12-02 21:44:14 +0100

    obtenir comentaris per issue id

--- aswissues/api_views/api_views.py
+++ aswissues/api_views/api_views.py
@@ -1,7 +1,7 @@
 from ..models import Issue, Comment
 from rest_framework import viewsets
 from ..api_serializers.serializers import IssueSerializer, CommentSerializer
-from rest_framework.response import Response 
+from rest_framework.response import Response
 
 class IssueViewSet(viewsets.ModelViewSet):
     """
@@ -10,9 +10,20 @@ class IssueViewSet(viewsets.ModelViewSet):
     queryset = Issue.objects.all()
     serializer_class = IssueSerializer
 
+
+
 class CommentViewSet(viewsets.ModelViewSet):
     """
     API endpoint that allows a single comment to be viewed or edited.
     """
-    queryset = Comment.objects.all()
     serializer_class = CommentSerializer
+    def get_queryset(self):
+        """
+        Optionally restricts the returned purchases to a given user,
+        by filtering against a `username` query parameter in the URL.
+        """
+        queryset = Comment.objects.all()
+        nissue = self.request.query_params.get('issue', None)
+        if nissue is not None:
+            queryset = queryset.filter(issue__id=nissue)
+        return queryset
