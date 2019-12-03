800c9953b449da5c683369077d8711505cb23002
atsuky <andreugallofre@gmail.com>
2019-12-03 21:20:04 +0100

    Add the information about the user that's creating the object

--- aswissues/api_views/api_views.py
+++ aswissues/api_views/api_views.py
@@ -3,6 +3,7 @@ from rest_framework import viewsets
 from ..api_serializers.serializers import IssueSerializer, CommentSerializer, AttachmentSerializer
 from rest_framework.response import Response
 
+
 class IssueViewSet(viewsets.ModelViewSet):
     """
     API endpoint that allows issues to be viewed or edited.
@@ -10,6 +11,8 @@ class IssueViewSet(viewsets.ModelViewSet):
     queryset = Issue.objects.all()
     serializer_class = IssueSerializer
 
+    def perform_create(self, serializer):
+        serializer.save(creator=self.request.user)
 
 
 class CommentViewSet(viewsets.ModelViewSet):
@@ -17,6 +20,7 @@ class CommentViewSet(viewsets.ModelViewSet):
     API endpoint that allows a single comment to be viewed or edited.
     """
     serializer_class = CommentSerializer
+
     def get_queryset(self):
         """
         Optionally restricts the returned purchases to a given user,
@@ -28,8 +32,12 @@ class CommentViewSet(viewsets.ModelViewSet):
             queryset = queryset.filter(issue__id=nissue)
         return queryset
 
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+
 class AttachmentViewSet(viewsets.ModelViewSet):
-    #queryset = Attachment.objects.all()
+    # queryset = Attachment.objects.all()
     serializer_class = AttachmentSerializer
 
     def get_queryset(self):
@@ -37,9 +45,12 @@ class AttachmentViewSet(viewsets.ModelViewSet):
         Optionally restricts the returned purchases to a given user,
         by filtering against a `username` query parameter in the URL.
         """
-        #serializer_class = AttachmentSerializer(Attachment, context={"request":self.request})
+        # serializer_class = AttachmentSerializer(Attachment, context={"request":self.request})
         queryset = Attachment.objects.all()
         nissue = self.request.query_params.get('issue', None)
         if nissue is not None:
             queryset = queryset.filter(issue__id=nissue)
         return queryset
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
