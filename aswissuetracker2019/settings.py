bf2d06f573fe9f76ccb726f9d43f619610b18dc7
Auri <ainaa.105@gmail.com>
2019-12-18 09:28:57 +0100

    revert

--- aswissuetracker2019/settings.py
+++ aswissuetracker2019/settings.py
@@ -65,7 +65,6 @@ SWAGGER_SETTINGS = {
 REST_FRAMEWORK = {
     'DEFAULT_SCHEMA_CLASS': 'rest_framework.schemas.coreapi.AutoSchema',
     'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework.authentication.SessionAuthentication',
         'rest_framework.authentication.TokenAuthentication',  # <-- And here
     ],
     'DEFAULT_PERMISSION_CLASSES': [
