d9087ad422b6f97de8854bc936943001387d168e
Andreu Gallofré <andreu.gallofre@capside.com>
2020-01-14 14:43:37 +0100

    Change google auth

--- aswissuetracker2019/settings.py
+++ aswissuetracker2019/settings.py
@@ -183,8 +183,7 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 prod_db = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(prod_db)
 
-SOCIAL_AUTH_GOOGLE_OAUTH2_KEY = '244422922408-kdob86ugb85gg60q80m2jit3kifij32v.apps.googleusercontent.com
-'
+SOCIAL_AUTH_GOOGLE_OAUTH2_KEY = '244422922408-kdob86ugb85gg60q80m2jit3kifij32v.apps.googleusercontent.com'
 SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET = 'NUDxO041vgYcsuf9BNprNoo5'
 
 LOGIN_URL = '/auth/login/google-oauth2/'
